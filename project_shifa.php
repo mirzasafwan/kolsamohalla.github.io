6b4d5790bebbe11f9f6dc4609014f8cf48709e5f
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-04-05 00:07:01 +0530

    init

---
+++ project_shifa.php
@@ -0,0 +1,167 @@
+<!DOCTYPE HTML>
+<html class="no-js">
+<?php include 'head.php'; ?>
+<style>
+    .lgray-bg.padding-tb75 {
+        background: transparent;
+    }
+    @media screen and (min-width:1024px){
+    .page-banner{
+            min-height:500px;
+        }
+        .block-title{
+            margin-bottom: 160px;
+        }
+}
+</style>
+
+<body>
+    <!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
+    <div class="body">
+        <!-- Site Header Wrapper -->
+        <div class="site-header-wrapper">
+            <!-- Site Header -->
+            <?php include 'navbar.php'; ?>
+        </div>
+        <!-- Hero Area -->
+        <div class="hero-area">
+            <div class="page-banner parallax overlay" style="background-image:url(images/assets/banner/project_shifa.jpg);">
+                <div class="container">
+                    <div class="page-banner-text">
+                        <h1 class="block-title">Project Shifa Medical</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Our Project</a></li>
+                    <li class="breadcrumb-item active">Project Shifa</li>
+                </ol>
+            </nav>
+        </div>
+        <div class="section-1 inner_section wow fadeInUp">
+            <div class="container">
+                <h3 class="text-center">"If there is any healing in your medicines, then it is in cupping, a gulp of honey or branding with fire (cauterization) that suits the ailment, but I don't like to be (cauterized) branded with fire."</h3>
+                <p class="text-center"> - Prophet Muhammad (S.A.W)</p>
+            </div>
+        </div>
+
+        <div class="container padding-tb75">
+            <li class="item">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="story-slider-content">
+                            <div class="story-slider-table">
+                                <div class="story-slider-cell">
+                                    <blockquote>
+                                        <h3>PROJECT SHIFA (MEDICAL HELP)</h3>
+                                        <p class="justify">Tata Pipes, one of the first brands of plumbing pipes in India, has played a crucial role in modernisation of the country. These pipes are mainly used for carrying liquids and low pressure gases. Plumbing, irrigation, cold storage, HVAC, fire-fighting and idlers too come under this application.</p>
+                                        <p class="justify">As we aim to achieve goals which no other Jamats or community has ever tried to achieve.</p>
+                                    </blockquote>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/assets/inner_section/Project_Shifa/project_shifa-1.jpg" alt="" class="img-responsive">
+                    </div>
+                </div>
+            </li>
+        </div>
+
+        <div class="padding-tb75 parallax overlay" style="background-image:url(images/assets/inner_section/Project_Shifa/cover.jpg);">
+            <div class="lgray-bg padding-tb75 relative">
+                <div class="container ">
+                    <div class="text-align-center">
+                        <h2 class="white">We aim to do the following.</h2>
+                    </div>
+                    <div class="carousel-wrapper">
+                        <div class="row ">
+                            <ul class="owl-carousel carousel-fw" id="causes-slider" data-columns="4" data-autoplay="" data-pagination="no" data-arrows="yes" data-single-item="no" data-items-desktop="4" data-items-desktop-small="2" data-items-tablet="2" data-items-mobile="1">
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Project_Shifa/icon/dialysis.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Dialysis<br> Centre </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Project_Shifa/icon/chemopathy.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Chemotherapy
+                                                    <br> Centre
+                                                </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Project_Shifa/icon/medicine.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Free/Subsidised<br> Medicine Help
+                                                </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Project_Shifa/icon/diagnostic.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Diagnostic <br>Center
+                                                </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <?php include 'footer.php'; ?>
+    </div>
+    <script src="js/jquery-2.1.3.min.js"></script> <!-- Jquery Library Call -->
+    <script src="vendor/magnific/jquery.magnific-popup.min.js"></script> <!-- PrettyPhoto Plugin -->
+    <script src="js/ui-plugins.js"></script> <!-- UI Plugins -->
+    <script src="js/helper-plugins.js"></script> <!-- Helper Plugins -->
+    <script src="vendor/owl-carousel/js/owl.carousel.min.js"></script> <!-- Owl Carousel -->
+    <script src="js/bootstrap.js"></script> <!-- UI -->
+    <script src="js/init.js"></script> <!-- All Scripts -->
+    <script src="vendor/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
+    <script src="js/circle-progress.js"></script> <!-- Circle Progress Bars -->
+    <script src="js/wow.min.js"></script>
+    <script src="js/wow.js"></script>
+</body>
+
+</html>
---
+++ women_empowerment.php
@@ -0,0 +1,237 @@
+<!DOCTYPE HTML>
+<html class="no-js">
+<?php include 'head.php'; ?>
+<style>
+    .lgray-bg.padding-tb75 {
+        background: transparent;
+    }
+    @media screen and (min-width:1280px){
+    .page-banner{
+            min-height:500px;
+        }
+        .block-title{
+            margin-bottom: 160px;
+        }
+}
+</style>
+
+<body>
+    <!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
+    <div class="body">
+        <!-- Site Header Wrapper -->
+        <div class="site-header-wrapper">
+            <!-- Site Header -->
+            <?php include 'navbar.php'; ?>
+        </div>
+        <!-- Hero Area -->
+        <div class="hero-area">
+            <div class="page-banner parallax overlay" style="background-image:url(images/assets/banner/women_empowerment.jpg);">
+                <div class="container">
+                    <!-- <div class="page-banner-text">
+                        <h1 class="block-title">Women Empowerment</h1>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Our Project</a></li>
+                    <li class="breadcrumb-item active">Women Empowerment</li>
+                </ol>
+            </nav>
+        </div>
+        <div class="section-1 inner_section wow fadeInUp">
+            <div class="container">
+                <h3 class="text-center">"You educate a man; you educate a man. You educate a Woman, you educate a generation."</h3>
+            </div>
+        </div>
+        <div class="container-fluid padding-tb75">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="story-slider-content">
+                        <div class="story-slider-table mt-20">
+                            <div class="story-slider-cell">
+                                <blockquote>
+                                    <h3>Women Empowerment</h3>
+                                    <p class="justify">We believe that it is our moral responsibility to empower our sisters as we believe that an empowered woman is powerful beyond measure and beautiful beyond description. Our sisters have always made our community proud and it's time we empower them like never before.</p>
+                                    <p class="justify">Your funds will be directly used to empower them as we look to execute Honahar Beti initiative in our Jamat Khana.</p>
+                                    <p class="justify">We don't want to empower them with just education but we also want to give them a platform to earn. Keeping this in mind, we will do Kolsa Bazaar initiative for them too. </p>
+                                </blockquote>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="images/assets/inner_section/Women_Empowerment/women empowerment-1.jpg" alt="" class="img-responsive">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="padding-tb75 parallax overlay" style="background-image:url(images/assets/inner_section/Women_Empowerment/cover.jpg);">
+            <div class="lgray-bg padding-tb75 relative">
+                <div class="container ">
+                    <div class="text-align-center">
+                        <h2 class="white">Courses Under Honahar Beti</h2>
+                    </div>
+                    <div class="carousel-wrapper">
+                        <div class="row ">
+                            <ul class="owl-carousel carousel-fw" id="causes-slider" data-columns="3" data-autoplay="" data-pagination="no" data-arrows="yes" data-single-item="no" data-items-desktop="3" data-items-desktop-small="2" data-items-tablet="2" data-items-mobile="1">
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/cooking.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Cooking <br> Classes </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/makeup-artist.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Make-up Artist <br> Training </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/stitching.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Stitching <br> Class</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class=" carousel-wrapper">
+                        <div class="row ">
+                            <ul class="owl-carousel carousel-fw" id="causes-slider" data-columns="3" data-autoplay="" data-pagination="no" data-arrows="yes" data-single-item="no" data-items-desktop="3" data-items-desktop-small="2" data-items-tablet="2" data-items-mobile="1">
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/computer.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Computer <br> Training</a></h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/mehndi-design.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Mehendi <br> Application</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/baking_classes.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Baking and Cake <br> Making Course</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class=" carousel-wrapper">
+                        <div class="row ">
+                            <ul class="owl-carousel carousel-fw" id="causes-slider" data-columns="3" data-autoplay="" data-pagination="no" data-arrows="yes" data-single-item="no" data-items-desktop="3" data-items-desktop-small="2" data-items-tablet="2" data-items-mobile="1">
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/fashion designing.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Fashion Designing <br> Basic Course</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/yoga.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">Yoga <br> Classes</a></h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="item">
+                                    <div class="grid-item cause-grid-item small-business format-standard">
+                                        <div class="grid-item-inner">
+                                            <a href="team">
+                                                <img src="images/assets/inner_section/Women_Empowerment/icon/english.jpg" alt="">
+                                            </a>
+                                            <div class="grid-item-content">
+                                               
+                                                <h3 class="post-title">English Speaking <br> Course</a></h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <?php include 'footer.php'; ?>
+    </div>
+    <script src="js/jquery-2.1.3.min.js"></script> <!-- Jquery Library Call -->
+    <script src="vendor/magnific/jquery.magnific-popup.min.js"></script> <!-- PrettyPhoto Plugin -->
+    <script src="js/ui-plugins.js"></script> <!-- UI Plugins -->
+    <script src="js/helper-plugins.js"></script> <!-- Helper Plugins -->
+    <script src="vendor/owl-carousel/js/owl.carousel.min.js"></script> <!-- Owl Carousel -->
+    <script src="js/bootstrap.js"></script> <!-- UI -->
+    <script src="js/init.js"></script> <!-- All Scripts -->
+    <script src="vendor/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
+    <script src="js/circle-progress.js"></script> <!-- Circle Progress Bars -->
+    <script src="js/wow.min.js"></script>
+    <script src="js/wow.js"></script>
+</body>
+
+</html>
